e0ad64ee059267856332fb9df123d46140a2c6df
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2024-08-26 11:10:33 +0530

    blog

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -90,10 +90,28 @@ export class Service {
   // file upload service
   async uploadFile(file) {
     try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
     } catch (error) {
       console.log("Appwrite serive :: uploadFile :: error", error);
       return false;
     }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+    } catch (error) {
+      console.log("Appwrite serive :: deleteFile :: error", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
   }
 }
 
